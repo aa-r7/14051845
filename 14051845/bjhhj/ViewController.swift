275ca4dec4eafb93c0a9276b0575684ce649911a
aa-r7 <36970775+aa-r7@users.noreply.github.com>
2018-04-14 21:33:06 +0100

    Add files via upload

---
+++ 14051845/bjhhj/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  bjhhj
+//
+//  Created by abdulla abdulla on 4/7/18.
+//  Copyright © 2018 abdulla abdulla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var roadImage: UIImageView!
+    
+    @IBOutlet weak var carview0: drag_car!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        var imageArray: [UIImage]!
+        
+        imageArray = [UIImage(named: "road1.png")!,
+                      UIImage(named: "road2.png")!,
+                      UIImage(named: "road3.png")!,
+                      UIImage(named: "road4.png")!,
+                      UIImage(named: "road5.png")!,
+                      UIImage(named: "road6.png")!,
+                      UIImage(named: "road7.png")!,
+                      UIImage(named: "road8.png")!,
+                      UIImage(named: "road9.png")!,
+                      UIImage(named: "road10.png")!,
+                      UIImage(named: "road11.png")!,
+                      UIImage(named: "road12.png")!,
+                      UIImage(named: "road13.png")!,
+                      UIImage(named: "road14.png")!,
+                      UIImage(named: "road15.png")!,
+                      UIImage(named: "road16.png")!,
+                      UIImage(named: "road17.png")!,
+                      UIImage(named: "road18.png")!,
+                      UIImage(named: "road19.png")!,
+                      UIImage(named: "road20.png")!,]
+        
+        roadImage.image = UIImage.animatedImage(with: imageArray, duration: 1)
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 14051845/bjhhj/drag_car.swift
@@ -0,0 +1,29 @@
+//
+//  drag_car.swift
+//  bjhhj
+//
+//  Created by abdulla abdulla on 4/7/18.
+//  Copyright © 2018 abdulla abdulla. All rights reserved.
+//
+
+import UIKit
+
+class drag_car: UIImageView {
+    
+    var startLocation: CGPoint?
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        startLocation = touches.first?.location(in: self)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let currentLocation = touches.first?.location(in: self)
+        let dx = currentLocation!.x - startLocation!.x
+        let dy = currentLocation!.y - startLocation!.y
+        
+        self.center = CGPoint(x:self.center.x+dx, y:self.center.y+dy)
+
+}
+}
