96d0e196bc6d071b8fc41d9d2320b67bb2fe50ce
aa-r7 <36970775+aa-r7@users.noreply.github.com>
2018-04-15 22:23:33 +0100

    Add files via upload

--- 14051845/bjhhj/ViewController.swift
+++ 14051845/bjhhj/ViewController.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol subviewDelegate {
+    func changeSomething()
+}
 
+
+class ViewController: UIViewController, subviewDelegate {
+    
+    func changeSomething() {
+        
+
+        collisionBehavior.addBoundary(withIdentifier: "barrier" as
+            NSCopying, for: UIBezierPath(rect: carView0.frame))
+        collisionBehavior.removeAllBoundaries()
+
+    }
+    
+    var collisionBehavior: UICollisionBehavior!
+    var dynamicAnimator: UIDynamicAnimator!
+    var dynamicItemBehavior: UIDynamicItemBehavior!
+    
     @IBOutlet weak var roadImage: UIImageView!
     
-    @IBOutlet weak var carview0: drag_car!
+    @IBOutlet weak var carView0: drag_car!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+
+        //road
         var imageArray: [UIImage]!
-        
         imageArray = [UIImage(named: "road1.png")!,
                       UIImage(named: "road2.png")!,
                       UIImage(named: "road3.png")!,
@@ -38,17 +58,58 @@ class ViewController: UIViewController {
                       UIImage(named: "road18.png")!,
                       UIImage(named: "road19.png")!,
                       UIImage(named: "road20.png")!,]
-        
         roadImage.image = UIImage.animatedImage(with: imageArray, duration: 1)
         
         
+
+            let randomNx = arc4random_uniform(201) + 40
+
+            var carView1 = UIImageView(image: nil)
+   
+            carView1.frame = CGRect(x: Int(randomNx), y: 50, width: 30, height: 50)
+
+            carView1.image = UIImage(named: "car1.png")
+       
+        self.view.addSubview(carView1)
+        
+        let randomNx1 = arc4random_uniform(201) + 40
+        
+        var carView2 = UIImageView(image: nil)
+        
+        carView2.frame = CGRect(x: Int(randomNx1), y: 50, width: 30, height: 50)
+        
+        carView2.image = UIImage(named: "car2.png")
+        
+        self.view.addSubview(carView2)
+                
         
         
+
+    
+        let randomN = arc4random_uniform(75) + 200
+        let randomN1 = arc4random_uniform(75) + 200
+
+        
+        dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+        
+        dynamicItemBehavior = UIDynamicItemBehavior(items: [carView1, carView2])
+        self.dynamicItemBehavior.addLinearVelocity(CGPoint(x: 0, y: Int(randomN)), for: carView1)
+        self.dynamicItemBehavior.addLinearVelocity(CGPoint(x: 0, y: Int(randomN1)), for: carView2)
+
         
         
+        dynamicAnimator.addBehavior(dynamicItemBehavior)
         
+    
         
-    }
+        collisionBehavior = UICollisionBehavior(items: [carView1, carView2, carView0 ])
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = false
+        dynamicAnimator.addBehavior(collisionBehavior)
+        
+        collisionBehavior.removeAllBoundaries()
+
+        }
+        
     
     
     
